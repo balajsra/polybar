b948d478596d6b7b9a9841cb4fe407d267092fc8
Sravan Balaji <balajsra@umich.edu>
2020-11-24 14:45:55 -0500

    Initial Bar
    - Add example config
    - Add Palmdrop's launch script

---
+++ launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+rm /tmp/polybar.pids
+
+polybar -c ~/.config/polybar/config.ini example &
+echo "$!" >>/tmp/polybar.pids
