c3f568eca9e6cb966d463fa6c30dd6ae076c4f2b
Sravan Balaji <balajsra@umich.edu>
2020-11-25 11:11:17 -0500

    Module Colors & Multi-Monitor Bars
    - Make monitor and tray position environment variables
    - Reduce hieght to 25
    - Add padding between last module and systray
    - Add vertical offset for fonts
    - Add powermenu module
    - Reorder right modules
    - Add "|" separator
    - Reverse tag scrolling
    - Reduce max title length
    - Change module colors
    - Reduce media-playing module interval to reduce CPU load
    - Parameterize launch script to launch bar on multi-monitor with only one systray

--- launch.sh
+++ launch.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+BAR="mybar"
+CONFIG="~/.config/polybar/config.ini"
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+TRAY_POS="right"
+
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 rm /tmp/polybar.pids
+sleep 1
 
-polybar -c ~/.config/polybar/config.ini mybar &
+# Launch on primary monitor
+MONITOR=$PRIMARY TRAY_POS=$TRAY_POS polybar --reload -c $CONFIG $BAR &
+sleep 1
+
+# Launch on all other monitors
+for m in $OTHERS; do
+    MONITOR=$m TRAY_POS=none polybar --reload -c $CONFIG $BAR &
+done
+
 echo "$!" >>/tmp/polybar.pids
