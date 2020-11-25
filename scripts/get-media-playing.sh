4ff86b3818dade7d3def1f5e9ae9d531f2e0853a
Sravan Balaji <balajsra@umich.edu>
2020-11-25 17:03:08 -0500

    Maxlen, Separator, & Media Playing Changes
    - Change separator to space
    - Add maxlen parameter from xresources
    - Replace media-playing text status with icon and move to front

--- scripts/get-media-playing.sh
+++ scripts/get-media-playing.sh
@@ -3,7 +3,17 @@
 mediaStatus=$(playerctl --player=playerctld metadata 2>&1)
 
 if [[ "$mediaStatus" == "No player could handle this command" ]]; then
-	echo "No Player Found"
+	echo "  N/A"
 else
-	echo "$(playerctl --player=playerctld metadata --format '{{ artist }} - {{ title }} | {{ uc(status) }}')"
+	artist=$(playerctl --player=playerctld metadata --format '{{ artist }}')
+	title=$(playerctl --player=playerctld metadata --format '{{ title }}')
+	status=$(playerctl --player=playerctld metadata --format '{{ status }}')
+
+	if [[ $status == "Paused" ]]; then
+		status_icon=" "
+	elif [[ $status == "Playing" ]]; then
+		status_icon=" "
+	fi
+
+	echo "$status_icon $artist - $title"
 fi
