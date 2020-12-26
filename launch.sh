6ba9bf2b55d82b60fdb68af762385a9ecd922858
Sravan Balaji <balajsra@umich.edu>
2020-12-26 16:02:12 -0500

    Systray Fix for NVIDIA Optimus
    - Fixed issue where single monitor computer would not show systray if monitor is not primary when switching GPUs

--- launch.sh
+++ launch.sh
@@ -2,8 +2,8 @@
 
 BAR="mybar"
 CONFIG="~/.config/polybar/config.ini"
-PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
-OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+NUM_MONITORS=0
+CONNECTED_MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
 TRAY_POS="right"
 
 killall -q polybar
@@ -12,13 +12,25 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 rm /tmp/polybar.pids
 sleep 1
 
-# Launch on primary monitor
-MONITOR=$PRIMARY TRAY_POS=$TRAY_POS polybar --reload -c $CONFIG $BAR &
-sleep 1
-
-# Launch on all other monitors
-for m in $OTHERS; do
-    MONITOR=$m TRAY_POS=none polybar --reload -c $CONFIG $BAR &
+for m in $CONNECTED_MONITORS; do
+    let "NUM_MONITORS+=1"
 done
+
+if [ $NUM_MONITORS == 1 ]; then
+    # Launch on only monitor w/ systray
+    MONITOR=$CONNECTED_MONITORS TRAY_POS=$TRAY_POS polybar --reload -c $CONFIG $BAR &
+else
+    PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+    OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+    # Launch on primary monitor w/ systray
+    MONITOR=$PRIMARY TRAY_POS=$TRAY_POS polybar --reload -c $CONFIG $BAR &
+    sleep 1
+
+    # Launch on all other monitors w/o systray
+    for m in $OTHERS; do
+        MONITOR=$m TRAY_POS=none polybar --reload -c $CONFIG $BAR &
+    done
+fi
 
 echo "$!" >>/tmp/polybar.pids
