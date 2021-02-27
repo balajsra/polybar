6ae0ce38d34af2e56e797c465e0ee5357c3b0d4d
Sravan Balaji <balajsra@umich.edu>
2021-02-26 22:21:40 -0500

    Fix Dunst Notification Status Script
    - Only check dunst status if it is running

--- scripts/dunst-notification-status.sh
+++ scripts/dunst-notification-status.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-is_paused=$(dunstctl is-paused)
-
-if [[ $is_paused == 'false' ]]; then
-    status_icon=" on"
-elif [[ $is_paused == 'true' ]]; then
+# Check if dunst is running
+if pgrep -x "dunst" > /dev/null
+then
+    is_paused=$(dunstctl is-paused)
+    
+    if [[ $is_paused == 'false' ]]; then
+        status_icon=" on"
+    elif [[ $is_paused == 'true' ]]; then
+        status_icon=" off"
+    fi
+else
     status_icon=" off"
 fi
 
